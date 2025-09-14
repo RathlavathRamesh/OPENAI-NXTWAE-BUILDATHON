50d8f76d25746714795eb36d6eb90f4d136d8407
RathlavathRamesh <rameshrguktbasar@gmail.com>
2025-09-14 12:55:10 +0530

    added keys for testing

---
+++ utils/keys.py
@@ -0,0 +1,8 @@
+
+
+# GROQ_API_KEY=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
+# GROQ_ASR_MODEL=whisper-large-v3-turbo
+# GROQ_VISION_MODEL=meta-llama/llama-4-scout-17b-16e-instruct
+# STREAMLIT_SERVER_RUN_ON_SAVE=true
+# GEMINI_API_KEY=AIzaSyCo7gvEb-yosc1Onhhu6PDPAmsfdMnERCg
+# WEATHER_API_KEY=383b7dd28c2be589c115e7e7a084eef6
